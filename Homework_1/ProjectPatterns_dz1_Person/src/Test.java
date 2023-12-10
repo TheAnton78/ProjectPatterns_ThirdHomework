3ed9238e475e4f23274baa3dfcdfcb00dbc485f8
Shaa test <shvyrev.anton78@gmail.com>
2023-12-10 19:26:03 +0500

    FirstHomework2

--- Homework_1/ProjectPatterns_dz1_Person/src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String name = "Hshbj";
-        System.out.println(name.toCharArray());
-        System.out.println(Character.isUpperCase(name.toCharArray()[0]));
-    }
-}
